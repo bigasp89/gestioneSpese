8b224e25f6864ec96e77e6b8bdaba82a59309f26
bigasp89 <giacomo.bigazzi20@gmail.com>
2019-04-07 02:41:00 +0200

    1_add_menu_voice

--- app/src/main/java/com/example/gestionespese/MainActivity.java
+++ app/src/main/java/com/example/gestionespese/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.gestionespese;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.gestionespeses.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = " Activity = Home page";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +36,15 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+    //metodo che viene invocato quando un Item del menu viene selezionato
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int idVoce =item.getItemId();
+        String nome= (String) item.getTitle();
+
+        Log.i(TAG,"clickVoceMenu "+ nome);
+
+        return super.onOptionsItemSelected(item);
     }
 }
